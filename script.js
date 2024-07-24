0c01baea57a6068c26ff9498a3095907a693a14a
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-07-23 20:15:29 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.theme-controller').addEventListener('change', function() {
-  if(this.checked) {
-    document.documentElement.setAttribute('data-theme', 'dim')
-  } else {
-    document.documentElement.setAttribute('data-theme', 'emerald')
-  }
-});
