742c1c2845875ac1df4bfaa3f9194325db5b83e8
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-08-03 17:06:09 -0700

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,12 +1,9 @@
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-  console.log("Hamburger clicked");
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,3 +15,5 @@ module.exports = {
   },
   plugins: [require('daisyui')],
 }
+
+
