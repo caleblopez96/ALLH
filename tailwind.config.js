7dd864a9137f6547eee3fbd79077378bff099446
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-08-11 16:30:55 -0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./src/*.{html,js}",
-  "./src/pages/**/*.html",
+  "./src/**/*.{html,js}",
   "./index.html"
 ],
   theme: {
